aacc5f6939bd502c140d1af42f644c886f022259
Karolis Astrauka <astrauka@gmail.com>
2016-03-14 10:30:05 +0200

    Add spring support

--- rspec/spec_commands/spring.py
+++ rspec/spec_commands/spring.py
@@ -1,9 +1,16 @@
 import os
-from plugin_helpers.decorators import memoize
 
 class Spring(object):
   def __init__(self, context):
     self.context = context
 
   def result(self):
-    return None
+    if not self.context.from_settings("check_for", {}).get("spring"): return None
+    if self.spring_included(): return "spring"
+
+  def spring_included(self):
+    gemfile_path = self.context.gemfile_path()
+    if not gemfile_path: return None
+
+    return open(gemfile_path, 'r').read().find('spring') > 0
+
